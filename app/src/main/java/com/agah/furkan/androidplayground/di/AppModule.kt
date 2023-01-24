c5276aadba8ae25481826de7c740c3ee10aaf22c
agahfurkan <agahfurkan@gmail.com>
2023-01-24 23:29:57 +0300

    update dependencies, add workaround solution for localhost url

--- app/src/main/java/com/agah/furkan/androidplayground/di/AppModule.kt
+++ app/src/main/java/com/agah/furkan/androidplayground/di/AppModule.kt
@@ -16,12 +16,17 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import java.security.KeyStore
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+import javax.net.ssl.*
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -72,11 +77,54 @@ object AppModule {
     fun provideOkHttpClient(): OkHttpClient {
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+
+        val trustAllCerts = arrayOf<TrustManager>(
+            object : X509TrustManager {
+                @Throws(CertificateException::class)
+                override fun checkClientTrusted(
+                    chain: Array<X509Certificate?>?,
+                    authType: String?
+                ) {
+                }
+
+                @Throws(CertificateException::class)
+                override fun checkServerTrusted(
+                    chain: Array<X509Certificate?>?,
+                    authType: String?
+                ) {
+                }
+
+                override fun getAcceptedIssuers(): Array<X509Certificate?>? {
+                    return arrayOf()
+                }
+            }
+        )
+
+        // Install the all-trusting trust manager
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+        // Create an ssl socket factory with our all-trusting manager
+        val sslSocketFactory = sslContext.socketFactory
+        val trustManagerFactory: TrustManagerFactory =
+            TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+        trustManagerFactory.init(null as KeyStore?)
+        val trustManagers: Array<TrustManager> =
+            trustManagerFactory.trustManagers
+        check(!(trustManagers.size != 1 || trustManagers[0] !is X509TrustManager)) {
+            "Unexpected default trust managers:" + trustManagers.contentToString()
+        }
+
+        val trustManager =
+            trustManagers[0] as X509TrustManager
+
         return OkHttpClient.Builder()
-            .addInterceptor(interceptor)
             .addInterceptor(AuthHeaderInterceptor())
+            .addInterceptor(interceptor)
             .readTimeout(RestConstants.READ_TIMEOUT, TimeUnit.SECONDS)
             .writeTimeout(RestConstants.WRITE_TIMEOUT, TimeUnit.SECONDS)
+            .sslSocketFactory(sslSocketFactory, trustManager)
+            .hostnameVerifier { hostname: String?, session: SSLSession? -> true }
             .build()
     }
 
