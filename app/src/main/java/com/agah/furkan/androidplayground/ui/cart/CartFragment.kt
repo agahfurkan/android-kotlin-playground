d1ef0c87035c47b4c2ef89a7aec863609b8b733d
agahfurkan <agahfurkan@gmail.com>
2023-05-20 23:44:00 +0300

    refactor cart screen with constraintlayout

--- app/src/main/java/com/agah/furkan/androidplayground/ui/cart/CartFragment.kt
+++ app/src/main/java/com/agah/furkan/androidplayground/ui/cart/CartFragment.kt
@@ -7,15 +7,12 @@ import android.view.ViewGroup
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -28,6 +25,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -37,6 +35,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.fragment.app.activityViewModels
 import com.agah.furkan.androidplayground.R
 import com.agah.furkan.androidplayground.SharedViewModel
@@ -87,7 +86,7 @@ class CartFragment : BaseFragment(null) {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun CartScreen(cartList: List<Cart>, onCartItemRemoved: (Cart) -> Unit = {}) {
     AppTheme {
@@ -100,83 +99,104 @@ fun CartScreen(cartList: List<Cart>, onCartItemRemoved: (Cart) -> Unit = {}) {
             LazyColumn(contentPadding = padding, content = {
                 items(cartList.size) {
                     val item = cartList[it]
-                    Row(
+
+                    ConstraintLayout(
                         modifier = Modifier
                             .padding(16.dp)
                             .fillMaxSize()
                             .border(2.dp, Color.Black, RoundedCornerShape(16.dp))
                             .padding(vertical = 16.dp, horizontal = 8.dp)
                     ) {
+                        val (img, text1, text2, text3, row) = createRefs()
+
                         ImageBox(
                             Modifier
+                                .constrainAs(img) {
+                                    top.linkTo(parent.top)
+                                    start.linkTo(parent.start)
+                                    bottom.linkTo(parent.bottom)
+                                }
                                 .height(88.dp)
                                 .width(88.dp)
                         )
-                        Column(
+
+                        Text(
                             modifier = Modifier
-                                .height(88.dp)
-                                .padding(start = 8.dp)
+                                .constrainAs(text1) {
+                                    top.linkTo(parent.top)
+                                    start.linkTo(img.end)
+                                }
+                                .padding(vertical = 0.dp),
+                            text = item.productName,
+                            fontSize = 12.sp
+                        )
+                        Text(
+                            modifier = Modifier.constrainAs(text2) {
+                                top.linkTo(text1.bottom)
+                                start.linkTo(img.end)
+                            },
+                            text = item.productName,
+                            fontSize = 12.sp
+                        )
+                        Text(
+                            modifier = Modifier
+                                .constrainAs(text3) {
+                                    top.linkTo(text2.bottom)
+                                    start.linkTo(img.end)
+                                }
+                                .padding(vertical = 0.dp),
+                            text = item.productName,
+                            fontSize = 12.sp
+                        )
+
+                        Row(
+                            modifier = Modifier
+                                .constrainAs(row) {
+                                    linkTo(text3.bottom, parent.bottom, bias = 1f)
+                                    start.linkTo(img.end)
+                                    bottom.linkTo(parent.bottom)
+                                }
+                                .height(24.dp)
+                                .fillMaxHeight(),
+                            horizontalArrangement = Arrangement.spacedBy(6.dp),
                         ) {
-                            Text(
-                                modifier = Modifier.padding(vertical = 0.dp),
-                                text = item.productName,
-                                fontSize = 12.sp
-                            )
-                            Text(
-                                modifier = Modifier.wrapContentHeight(),
-                                text = item.productName,
-                                fontSize = 12.sp
-                            )
-                            Text(
-                                modifier = Modifier.padding(vertical = 0.dp),
-                                text = item.productName,
-                                fontSize = 12.sp
-                            )
-                            Spacer(modifier = Modifier.weight(1f))
-                            Row(
+                            IconButton(
                                 modifier = Modifier
                                     .height(24.dp)
-                                    .fillMaxHeight(),
-                                horizontalArrangement = Arrangement.spacedBy(6.dp),
+                                    .width(24.dp),
+                                onClick = {
+
+                                }) {
+                                Icon(
+                                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_minus),
+                                    contentDescription = ""
+                                )
+                            }
+                            Box(
+                                modifier = Modifier
+                                    .align(Alignment.CenterVertically)
+                                    .height(24.dp)
+                                    .width(24.dp)
+                                    .border(2.dp, Color.Black, RoundedCornerShape(32.dp))
                             ) {
-                                IconButton(
-                                    modifier = Modifier
-                                        .height(24.dp)
-                                        .width(24.dp),
-                                    onClick = {
+                                Text(
+                                    modifier = Modifier.align(Alignment.Center),
+                                    text = "5",
+                                    fontSize = 12.sp,
+                                    textAlign = TextAlign.Center,
+                                )
+                            }
+                            IconButton(
+                                modifier = Modifier
+                                    .height(24.dp)
+                                    .width(24.dp),
+                                onClick = {
 
-                                    }) {
-                                    Icon(
-                                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_minus),
-                                        contentDescription = ""
-                                    )
-                                }
-                                Box(
-                                    modifier = Modifier
-                                        .align(Alignment.CenterVertically)
-                                        .height(24.dp)
-                                        .width(24.dp)
-                                        .border(2.dp, Color.Black, RoundedCornerShape(32.dp))
-                                ) {
-                                    Text(
-                                        modifier = Modifier.align(Alignment.Center),
-                                        text = "5",
-                                        fontSize = 12.sp,
-                                        textAlign = TextAlign.Center,
-                                    )
-                                }
-                                IconButton(
-                                    modifier = Modifier
-                                        .height(24.dp)
-                                        .width(24.dp),
-                                    onClick = {
-
-                                    }) {
-                                    Icon(
-                                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_plus),
-                                        contentDescription = ""
-                                    )
-                                }
+                                }) {
+                                Icon(
+                                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_plus),
+                                    contentDescription = ""
+                                )
                             }
                         }
                     }
